fb6fd629f4202ba1b10652e12fc5ed288c3c6bd8
manager <manager@qq.com>
2018-06-27 14:47:11 +0800

    项目的配置&数据库的配置

--- manager.py
+++ manager.py
@@ -1,6 +1,22 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    """项目配置"""
+    DEBUG = True
+
+    # 为 Mysql 添加配置
+    SQLAlchemy_DATABASE_URL = 'mysql://root:mysql@127.0.0.1:3306/information'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 app = Flask(__name__)
+
+# 加载配置
+app.config.from_object(Config)
+# 初始化数据库
+db = SQLAlchemy(app)
 
 
 @app.route("/")
@@ -9,4 +25,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
