f111036bc8277c2835f7abba37d39c4d3374221d
manager <manager@qq.com>
2018-06-27 16:34:02 +0800

    集成数据库的迁移命令

--- manager.py
+++ manager.py
@@ -6,6 +6,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 # 作用:可以用来指定 session 保存的位置
 from flask_session import Session
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 
 class Config(object):
@@ -54,7 +55,9 @@ Session(app)
 # 6.设置成命令行执行代码的方式:manager
 # 作用:可以添加 数据库迁移 的功能,因为要用到命令的方式
 manager = Manager(app)
-
+# 7.数据库的迁移
+Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 @app.route("/")
 def index():
