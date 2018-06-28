684240ba68a97820a5117257f7aaece4d68efcbd
manager <manager@qq.com>
2018-06-28 11:43:43 +0800

    抽取APP的创建逻辑到业务逻辑的模块里面

---
+++ info/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask.ext.session import Session
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.wtf import CSRFProtect
+from redis import StrictRedis
+
+from information.config import Config
+
+
+app = Flask(__name__)
+# 1.加载配置
+app.config.from_object(Config)
+# 2.初始化数据库
+db = SQLAlchemy(app)
+# 3.初始化 redis 存储对象
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+# 4.开启当前项目的 CSRF 保护,只做服务器验证功能(所有的请求都会经过before_request请求勾子)
+CSRFProtect(app)
+# 5.设置session保存指定位置
+Session(app)
+
+
--- manager.py
+++ manager.py
@@ -8,20 +8,10 @@ from flask_session import Session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from config import Config
+from information.info import app ,db
 
 
-app = Flask(__name__)
 
-# 1.加载配置
-app.config.from_object(Config)
-# 2.初始化数据库
-db = SQLAlchemy(app)
-# 3.初始化 redis 存储对象
-redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-# 4.开启当前项目的 CSRF 保护,只做服务器验证功能(所有的请求都会经过before_request请求勾子)
-CSRFProtect(app)
-# 5.设置session保存指定位置
-Session(app)
 # 6.设置成命令行执行代码的方式:manager
 # 作用:可以添加 数据库迁移 的功能,因为要用到命令的方式
 manager = Manager(app)
