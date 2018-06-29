03db73ceea8043f3a98843d91d66732f6460381f
manager <manager@qq.com>
2018-06-29 14:59:40 +0800

    加载网站小图标(流程解析)

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -14,6 +14,8 @@ def index():
     return render_template('/news/index.html')
 
 
+# 在打开网页的时候，浏览器会默认去请求根路径+favicon.ico作网站标签的小图标
+# send_static_file 是 flask 去查找指定的静态文件所调用的方法
 @index_blu.route('/favicon.ico')
 def favicon():
     return current_app.send_static_file('news/favicon.ico')
