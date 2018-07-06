3d499deebd88efd43420438c218b11579cdebec7
manager <manager@qq.com>
2018-07-06 23:17:00 +0800

    收藏和取消收藏后端逻辑代码实现

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,13 +1,61 @@
-from flask import abort
+from flask import abort, jsonify
 from flask import current_app
 from flask import g
 from flask import render_template
+from flask import request
 
-
-from info import constants
+from info import constants, db
 from info.models import News
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
 from . import news_blu
+
+
+@news_blu.route("/news_collect", methods=['POST'])
+@user_login_data
+def news_collect():
+    """新闻收藏"""
+    # 0. 取出用户(装饰器)
+    user = g.user
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户不存在")
+
+    # 1. 接受参数
+    news_id = request.json.get("news_id")
+    action = request.json.get("action")
+
+    # 2. 判断参数
+    if not all([news_id,action]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    if action not in ["collect","cancel_collect"]:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    try:
+        news_id = int(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 3. 查询新闻,并判断新闻是否存在
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询数据失败")
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="新闻数据不存在")
+    if action == "cancel_collect":
+        # 4. 取消收藏
+        if news in user.collection_news:
+            user.collection_news.remove(news)
+    else:
+        # 4. 收藏新闻
+        if news not in user.collection_news:
+            user.collection_news.append(news)
+
+        return jsonify(errno=RET.DBERR, errmsg="保存失败")
 
 
 @news_blu.route('/<int:news_id>')
