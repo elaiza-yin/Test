f878431e1ddcaf21d85f66ecac513aa5bc0630d1
manager <manager@qq.com>
2018-07-09 22:26:52 +0800

    个人中心的新闻发布模板渲染&新闻发布前后端逻辑的实现

--- info/static/news/js/user_news_release.js
+++ info/static/news/js/user_news_release.js
@@ -3,16 +3,27 @@ function getCookie(name) {
     return r ? r[1] : undefined;
 }
 
-
 $(function () {
-
     $(".release_form").submit(function (e) {
-        e.preventDefault()
-
-        // TODO 发布完毕之后需要选中我的发布新闻
-        // // 选中索引为6的左边单菜单
-        // window.parent.fnChangeMenu(6)
-        // // 滚动到顶部
-        // window.parent.scrollTo(0, 0)
+        e.preventDefault();
+        // 发布新闻
+        // 重点:会主动提交含有name标签的内容到后台----(相比较ajax发起请求,少了var  params = {} 手动传递参数到后端)
+        $(this).ajaxSubmit({
+            url: "/user/news_release",
+            type: "POST",
+            headers: {
+                "X-CSRFToken": getCookie('csrf_token')
+            },
+            success: function (resp) {
+                if (resp.errno == "0") {
+                    // 选中索引为6的左边单菜单
+                    window.parent.fnChangeMenu(6);
+                    // 滚动到顶部
+                    window.parent.scrollTo(0, 0)
+                }else {
+                    alert(resp.errmsg)
+                }
+            }
+        })
     })
-})
+});
